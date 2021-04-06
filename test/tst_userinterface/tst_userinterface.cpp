f0e7752d2150f9eecc631a47c665bf2fffb39edd
Jayson Nguyen <jvnyxc@umsystem.edu>
2021-04-06 12:45:28 -0500

    Update test suites

--- test/tst_userinterface/tst_userinterface.cpp
+++ test/tst_userinterface/tst_userinterface.cpp
@@ -12,6 +12,7 @@ void UserInterface::testOnePlayerScreen() {
 /*Test that the user will be sent to the one player screen*/
     Menu menu(&testScene);
     QSignalSpy spy(menu.returnSinglePlayerPushButton(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(menu.returnSinglePlayerPushButton(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
@@ -20,6 +21,7 @@ void UserInterface::testTwoPlayerScreen() {
 /*Test that the user will be sent to the two player screen*/
     Menu menu(&testScene);
     QSignalSpy spy(menu.returnTwoPlayerPushButton(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(menu.returnTwoPlayerPushButton(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
@@ -28,6 +30,7 @@ void UserInterface::testTutorialScreen() {
 /*Test that the user will be sent to the tutorial screen*/
     Menu menu(&testScene);
     QSignalSpy spy(menu.returnTutorialPushButton(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(menu.returnTutorialPushButton(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
@@ -36,6 +39,7 @@ void UserInterface::testQuitButton() {
 /*Test that the user can quit out of the game*/
     Menu menu(&testScene);
     QSignalSpy spy(menu.returnQuitButton(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(menu.returnQuitButton(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
@@ -44,6 +48,7 @@ void UserInterface::testOnePlayerMainMenuButton() {
 /*Test that the user go back to the main menu from one player mode*/
     SinglePlayerGame computerGame(&testScene, true);
     QSignalSpy spy(computerGame.returnMainMenu(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(computerGame.returnMainMenu(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
@@ -52,6 +57,7 @@ void UserInterface::testTwoPlayerMainMenuButton() {
 /*Test that the user go back to the main menu from two player mode*/
     Game game(&testScene);
     QSignalSpy spy(game.returnMainMenu(), SIGNAL(clicked()));
+    QCOMPARE(spy.count(), 0);
     QTest::mouseClick(game.returnMainMenu(), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
     QCOMPARE(spy.count(), 1);
 }
