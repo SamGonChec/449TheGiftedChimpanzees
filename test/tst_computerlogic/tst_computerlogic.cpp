4f3ef5de6f85ef136b9cc76adbe55be03fcd4717
Jayson Nguyen <jvnyxc@umsystem.edu>
2021-05-04 16:34:05 -0500

    Add testing for computer place a piece and computer blocking a mill

--- test/tst_computerlogic/tst_computerlogic.cpp
+++ test/tst_computerlogic/tst_computerlogic.cpp
@@ -1,35 +1,28 @@
-#include <QtTest>
+#include "tst_computerlogic.h"
 
-// add necessary includes here
-
-class computerlogic : public QObject
-{
-    Q_OBJECT
-
-public:
-    computerlogic();
-    ~computerlogic();
-
-private slots:
-    void test_case1();
-
-};
-
-computerlogic::computerlogic()
-{
-
+void ComputerLogic::testComputerPlacePiece() {
+    SinglePlayerGame game(&testScene, true);
+    std::vector<Space*> spaceList = game.getSpaceList();
+    for(int i = 0; i < 24; i++) {
+        if(spaceList[i]->isOccupied() == true && spaceList[i]->hasWhitePiece() == true)
+            QCOMPARE(game.getSpace(i)->isOccupied(), true);
+    }
 }
 
-computerlogic::~computerlogic()
-{
-
+void ComputerLogic::testComputerPlayerBlock() {
+    SinglePlayerGame game(&testScene, false);
+    int blockSpace;
+    QTest::mousePress(game.getSpace(22), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
+    if(game.getSpace(21)->isOccupied() == true) {
+        QTest::mousePress(game.getSpace(18), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
+        blockSpace = 16;
+    }
+    else {
+        QTest::mousePress(game.getSpace(23), Qt::LeftButton, Qt::KeyboardModifiers(), QPoint(10, 10));
+        blockSpace = 21;
+    }
+    QCOMPARE(game.getSpace(blockSpace)->hasBlackPiece(), true);
 }
 
-void computerlogic::test_case1()
-{
 
-}
-
-QTEST_APPLESS_MAIN(computerlogic)
-
-#include "tst_computerlogic.moc"
+QTEST_MAIN(ComputerLogic)
